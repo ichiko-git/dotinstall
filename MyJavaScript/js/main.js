e4999b2ce3a10dce793ea3af3fc681502735586d
いちこ <98368548+ichiko-git@users.noreply.github.com>
2025-05-06 15:30:07 +0900

    ドットインストール学習フォルダをアップロード

---
+++ MyJavaScript/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+{
+  // input →　text,textarea, select
+  // change → radio, checkbox
+
+  document.querySelector("button").addEventListener("click", () => {
+    document.querySelector("p").style.fontSize = "24px";
+  });
+}
